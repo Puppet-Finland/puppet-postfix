28e17bf309ed680707f427109ef5a0315809d6b0
Samuli Seppänen <samuli.seppanen@gmail.com>
2013-12-06 20:33:11 +0200

    Made monit start/stop rules OS-agnostic
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/params.pp
+++ manifests/params.pp
@@ -13,17 +13,14 @@ class postfix::params {
             $daemon_directory = '/usr/libexec/postfix'
             $command_directory = '/usr/sbin'
             $service_name = 'postfix'
-            $service_command = "/sbin/service $service_name"
-        }
-        'Suse': {
-            $package_name = 'postfix'
-            $main_cf = '/etc/postfix/main.cf'
-            $alias_maps = 'hash:/etc/aliases'
-            $alias_database = 'hash:/etc/aliases'
-            $daemon_directory = '/usr/lib/postfix'
-            $command_directory = '/usr/sbin'
-            $service_name = 'postfix'
-            $service_command = "/sbin/service $service_name"
+ 
+            if $::operatingsystem == 'Fedora' {
+                $service_start = "/usr/bin/systemctl start ${service_name}.service"
+                $service_stop = "/usr/bin/systemctl stop ${service_name}.service"
+            } else {
+                $service_start = "/sbin/service $service_name start"
+                $service_stop = "/sbin/service $service_name stop"
+            }
         }
         'Debian': {
             $package_name = 'postfix'
@@ -33,7 +30,8 @@ class postfix::params {
             $daemon_directory = '/usr/lib/postfix'
             $command_directory = '/usr/sbin'
             $service_name = 'postfix'
-            $service_command = "/usr/sbin/service $service_name"
+            $service_start = "/usr/sbin/service $service_name start"
+            $service_stop = "/usr/sbin/service $service_name stop"
         }
         'FreeBSD': {
             $package_name = 'mail/postfix'
@@ -43,7 +41,8 @@ class postfix::params {
             $daemon_directory = '/usr/local/libexec/postfix'
             $command_directory = '/usr/local/sbin'
             $service_name = 'postfix'
-            $service_command = "/usr/local/etc/rc.d/$service_name"
+            $service_start = "/usr/local/etc/rc.d/$service_name start"
+            $service_stop = "/usr/local/etc/rc.d/$service_name stop"
         }
         default: {
             $package_name = 'postfix'
@@ -53,7 +52,8 @@ class postfix::params {
             $daemon_directory = '/usr/lib/postfix'
             $command_directory = '/usr/sbin'
             $service_name = 'postfix'
-            $service_command = "/usr/sbin/service $service_name"
+            $service_start = "/usr/sbin/service $service_name start"
+            $service_stop = "/usr/sbin/service $service_name stop"
         }
     }
 }
